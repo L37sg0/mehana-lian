207192bbcf4bcbb138175258b61867ac6bd70688
l37sg0 <petar_1v@abv.bg>
2023-12-06 14:09:16 +0200

    [#1] Separate template into pages

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -8,9 +8,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('front/index.html.twig');
+        return $this->render('front/pages/home.html.twig');
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('front/pages/about.html.twig');
+    }
+
+    #[Route('/menu', name: 'menu')]
+    public function menu(): Response
+    {
+        return $this->render('front/pages/menu.html.twig');
+    }
+
+    #[Route('/events', name: 'events')]
+    public function events(): Response
+    {
+        return $this->render('front/pages/events.html.twig');
+    }
+
+    #[Route('/gallery', name: 'gallery')]
+    public function gallery(): Response
+    {
+        return $this->render('front/pages/gallery.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('front/pages/contact.html.twig');
     }
 }
