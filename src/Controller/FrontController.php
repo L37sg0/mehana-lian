6a5c1e2bf6e65b24caaaaaa0e9d3f0bf3fbfe396
l37sg0 <petar_1v@abv.bg>
2023-12-06 15:03:24 +0200

    [#1] Added global for website menu using EventSubscriber - now menu is dynamic from routes

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -8,37 +8,37 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'home', defaults: ['includeInWebsiteMenu' => true])]
     public function index(): Response
     {
         return $this->render('front/pages/home.html.twig');
     }
 
-    #[Route('/about', name: 'about')]
+    #[Route('/about', name: 'about', defaults: ['includeInWebsiteMenu' => true])]
     public function about(): Response
     {
         return $this->render('front/pages/about.html.twig');
     }
 
-    #[Route('/menu', name: 'menu')]
+    #[Route('/menu', name: 'menu', defaults: ['includeInWebsiteMenu' => true])]
     public function menu(): Response
     {
         return $this->render('front/pages/menu.html.twig');
     }
 
-    #[Route('/events', name: 'events')]
+    #[Route('/events', name: 'events', defaults: ['includeInWebsiteMenu' => true])]
     public function events(): Response
     {
         return $this->render('front/pages/events.html.twig');
     }
 
-    #[Route('/gallery', name: 'gallery')]
+    #[Route('/gallery', name: 'gallery', defaults: ['includeInWebsiteMenu' => true])]
     public function gallery(): Response
     {
         return $this->render('front/pages/gallery.html.twig');
     }
 
-    #[Route('/contact', name: 'contact')]
+    #[Route('/contact', name: 'contact', defaults: ['includeInWebsiteMenu' => true])]
     public function contact(): Response
     {
         return $this->render('front/pages/contact.html.twig');
