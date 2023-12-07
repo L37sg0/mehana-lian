b9035cb84b7a9c2648a30577a90639517e7255f2
l37sg0 <petar_1v@abv.bg>
2023-12-07 20:47:45 +0200

    [#2] Added other entities to admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Block;
+use App\Entity\Dish;
+use App\Entity\Event;
+use App\Entity\Image;
+use App\Entity\Menu;
 use App\Entity\Page;
+use App\Entity\Post;
+use App\Entity\Profile;
+use App\Entity\Review;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -33,6 +40,13 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Pages', 'fas fa book', Page::class);
         yield MenuItem::linkToCrud('Blocks', 'fas fa book', Block::class);
+        yield MenuItem::linkToCrud('Posts', 'fas fa book', Post::class);
+        yield MenuItem::linkToCrud('Images', 'fas fa book', Image::class);
+        yield MenuItem::linkToCrud('Menus', 'fas fa book', Menu::class);
+        yield MenuItem::linkToCrud('Dishes', 'fas fa book', Dish::class);
+        yield MenuItem::linkToCrud('Events', 'fas fa book', Event::class);
+        yield MenuItem::linkToCrud('Profiles', 'fas fa book', Profile::class);
+        yield MenuItem::linkToCrud('Reviews', 'fas fa book', Review::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
