280f4f70b2d0a0c5e6cff4a480b13801bf8f6984
l37sg0 <petar_1v@abv.bg>
2023-12-14 13:36:35 +0200

    [#21] Create global export action for menu items

--- src/Controller/Admin/MenuItemCrudController.php
+++ src/Controller/Admin/MenuItemCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -24,6 +27,18 @@ class MenuItemCrudController extends AbstractCrudController
             TextEditorField::new('ingredients'),
             NumberField::new('price'),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $exportAction = Action::new('export')
+            ->linkToCrudAction('export')
+            ->addCssClass('btn btn-success')
+            ->setIcon('fa fa-download')
+            ->createAsGlobalAction();
+
+        return parent::configureActions($actions)
+            ->add(Crud::PAGE_INDEX, $exportAction);
     }
 
 }
