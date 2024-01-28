b82e4ab8ba80b1adec3b2ec3621c8522b6d07fdb
l37sg0 <petar_1v@abv.bg>
2024-01-28 18:46:22 +0200

    [#41] Fix tests and workflows

--- src/DataFixtures/AdminFixtures.php
+++ src/DataFixtures/AdminFixtures.php
@@ -3,12 +3,20 @@
 namespace App\DataFixtures;
 
 use App\Entity\Admin;
+use App\Entity\ApiIntegration;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 ;
 
 class AdminFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
         $admin = new Admin();
@@ -16,7 +24,15 @@ class AdminFixtures extends Fixture
             ->setUsername('Administrator')
             ->setRoles(['ROLE_ADMIN'])
             ->setPassword('$2y$13$yfUpDE3isAV/gILH2PRpa.pqFagGzUQRtiwxnNfChBExfw84w5rcK');
+
+        $apiIntegration = (new ApiIntegration())
+            ->setClientId($_ENV['CLIENT_ID'])
+            ->setClientSecret($this->passwordHasher->hashPassword($admin, $_ENV['CLIENT_SECRET']))
+            ->setName('TEST Integration')
+            ->setUser($admin);
+
         $manager->persist($admin);
+        $manager->persist($apiIntegration);
 
         $manager->flush();
     }
